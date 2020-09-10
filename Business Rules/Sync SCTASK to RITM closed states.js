da344e2841abb927e3ad64f13438595627fc2376
Erica Almeida <32041698+almeidaerica@users.noreply.github.com>
2020-09-10 18:27:02 -0300

    Add files via upload

---
+++ Business Rules/Sync SCTASK to RITM closed states.js
@@ -0,0 +1,39 @@
+/*
+Advanced: checked
+When to run: after
+Update: checked
+Table: sc_req_item
+Filter Conditions: Stage | changes to | Closed Complete
+*/
+
+(function executeRule(current, previous /*null when async*/ ) {
+	
+    var gr = new GlideRecord('sc_task');
+    gr.addQuery('request_item', current.sys_id);
+    gr.query();
+    var isClosedC = true;
+    var isClosedI = true;
+    var isClosedS = true;
+
+    while (gr.next()) {
+        var cTaskState = gr.getValue('state');
+
+        if (cTaskState != 3 && cTaskState != 7 && isClosedC) {
+            isClosedC = false;
+        }
+
+        if (cTaskState != 7 && isClosedS) {
+            isClosedS = false;
+        }
+
+        if (cTaskState < 3 && isClosedI) {
+            isClosedI = false;
+        }
+    }
+    if (isClosedI) current.state = 4;
+    if (isClosedC) current.state = 3;
+    if (isClosedS) current.state = 7;
+
+    current.update();
+
+})(current, previous);
---
+++ Business Rules/Sync SCTASK to RITM pend, open and wip.js
@@ -0,0 +1,53 @@
+/*
+Advanced: checked
+When to run: after
+Update: checked
+Insert: checked
+Table: sc_task
+Filter Conditions: State | changes
+*/
+
+
+(function executeRule(current, previous /*null when async*/ ) {
+    var gr = new GlideRecord('sc_task');
+    gr.addQuery('request_item', current.parent);
+    gr.query();
+    var hasWIP = false;
+    var isPending = false;
+    var isOpen = true;
+    var hasOpen = false;
+
+    while (gr.next()) {
+        var cTaskState = gr.getValue('state');
+        var grReqItem = new GlideRecord('sc_req_item');
+
+        if (grReqItem.get(gr.parent)) {
+
+            if (cTaskState == '-5') {
+                isPending = true;
+                break;
+            }
+
+            if (cTaskState != 1 && isOpen) {
+                isOpen = false;
+            }
+
+            if (cTaskState == '2') {
+                hasWIP = true;
+            }
+
+            if (cTaskState == '1') {
+                hasOpen = true;
+            }
+
+        }
+    }
+
+    if (isPending) grReqItem.state = -5;
+    if (isOpen && !isPending) grReqItem.state = 1;
+    if ((hasWIP && !isPending) || (hasOpen && !isOpen)) grReqItem.state = 2;
+
+
+    grReqItem.update();
+
+})(current, previous);
